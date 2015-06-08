b00f705b7e3b7884481f4a8aff43a71e554697fe
Sergey Melekhin <sergey@emply.ru>
2015-06-08 17:25:23 +1000

    Removed stale module

--- pgdocgen/merge.py
+++
@@ -1,8 +0,0 @@
-'''Module with functions to merge objects read from database and JDOC from files'''
-from pgdocgen.ddlobject.db import DB
-from pgdocgen.parser import JDOC
-
-
-def merge_doc(jdoc,db):
-  '''Function to merge function descriptions from JDOC
-  and table descriptions from DB'''
