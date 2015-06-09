683be2a9c61d872ec6f7e7fec3664a42088b22ae
Sergey Melekhin <sergey@emply.ru>
2015-06-09 17:37:36 +1000

    ++Version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pgdocgen',
-    version='0.2',
+    version='0.3',
     packages=find_packages(),
     scripts=['pgdocgen/pgdocgen'],
     include_package_data=True,
